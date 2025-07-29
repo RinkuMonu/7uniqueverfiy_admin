7dc97d5c6483def40664234f449b2e788cae7b1c
vijaysingh7878 <vijaysinghshekhawat9928@gmail.com>
2025-07-29 15:06:51 +0530

    file

--- components/service/axiosInstance.tsx
+++ components/service/axiosInstance.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    // baseURL: 'https://api.7uniqueverfiy.com/api',
-    baseURL: 'http://localhost:5050/api', 
+    baseURL: 'https://api.7uniqueverfiy.com/api',
+    // baseURL: 'http://localhost:5050/api', 
     // headers: {
     //     'Content-Type': 'application/json',
     // },
